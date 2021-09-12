7d8cf66a6fd5ee2ca76d37e103c8c07d83655524
Tushar Jain <tusharjain4mar2002@gmail.com>
2021-09-13 00:31:13 +0530

    getContours fuunc added and tested.

--- virtualBrush/virtualBrush.cpp
+++ virtualBrush/virtualBrush.cpp
@@ -7,10 +7,36 @@ using namespace cv;
 //hmin,smin,vmin,hmax,smax,vmax
 vector<vector<int>> colorValues = { {24,83,136,30,255,252},//yellow
 									{101,69,123,115,170,255},//blue
-									{39,65,111,73,180,255},// green
-									{0,101,138,14,210,255}//red 
+									{77,52,111,88,238,255},// green
 };
-vector<Scalar> originalColorValues = { {255,255,0},{255,0,0},{0,255,0},{0,0,255} };
+vector<Scalar> originalColorValues = { {255,255,0},{255,0,0},{0,255,0} };
+Mat img;
+
+void getContours(Mat imgDilate) {
+	vector<vector<Point>> contours;
+	vector<Vec4i> hierarchy;
+
+	findContours(imgDilate, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
+
+	vector<vector<Point>> conPoly(contours.size());
+	vector<Rect> boundRect(contours.size());
+
+
+	for (int i = 0; i < contours.size(); i++) {
+		string objectType;
+		int area = contourArea(contours[i]);
+		cout << area << endl;
+		if (area > 1000) {
+			float peri = arcLength(contours[i], true);
+			approxPolyDP(contours[i], conPoly[i], 0.02 * peri, true);
+			drawContours(img, contours, i, Scalar(255, 0, 255), 2);
+			boundRect[i] = boundingRect(conPoly[i]);
+			rectangle(img, boundRect[i].tl(), boundRect[i].br(), Scalar(0, 255, 0), 3);
+
+		}
+
+	}
+}
 
 
 void findColor(Mat img) {
@@ -21,13 +47,14 @@ void findColor(Mat img) {
 		Scalar upper(colorValues[i][3], colorValues[i][4], colorValues[i][5]);
 		Mat mask;
 		inRange(imgHSV, lower, upper, mask);
-		imshow(to_string(i), mask);
+		//imshow(to_string(i), mask);
+		getContours(mask);
 	}
 }
 
 int main() {
 	VideoCapture cap(0);
-	Mat img;
+	
 	while (true) {
 		cap.read(img);
 		findColor(img);
